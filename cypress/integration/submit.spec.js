ca80da8e1b3c7d224a6e7a67b0fc62c25577abc4
scabrales <scabrales@mcclatchyservices.com>
2021-02-25 17:00:43 -0500

    Adding submit booking test with after hook

--- cypress/integration/submit.spec.js
+++ cypress/integration/submit.spec.js
@@ -2,40 +2,23 @@
 
 describe('Login feature', () => {
     beforeEach(() =>{
-        cy.visit('https://automationintesting.online/#/admin'); 
-        cy.fixture('login-details').as('data')
-    });
-    
-
-  /*  it('should create a booking successfully', function () {
-        cy.visit('https://automationintesting.online/#');
-        cy.get('#name').type('Pedro Pascal');
-        cy.get('#email').type('pedropascal@mail.com');
-        cy.get('#phone').type(3123123123123123);
-        cy.get('#subject').type('Our first test');
-        cy.get('#description').type('This is the message of my new booking enquiry');
-        cy.get('#submitContact').click();
-
-        cy.get(':nth-child(2) > div > h2').should("contain",'Pedro Pascal');
-
-        cy.visit('https://automationintesting.online/#/admin'); 
+        cy.visit('https://automationintesting.online/#'); 
         cy.fixture('login-details').as('data');
-       // const {username,password} = this.data;    // We are using destructuring
-        cy.login("username","password");
-        cy.get('#frontPageLink').should('be.visible');
+        cy.fixture('booking-info').as('booking');
     });
-    */
-    it('Test using @ to access the alias', () => {
 
- cy.visit('https://automationintesting.online/#');
-        cy.get('#name').type('Pedro Pascal');
-        cy.get('#email').type('pedropascal@mail.com');
-        cy.get('#phone').type(3123123123123123);
-        cy.get('#subject').type('Our first test');
-        cy.get('#description').type('This is the message of my new booking enquiry');
-        cy.get('#submitContact').click();
+     it('Test using @ to access the alias', function() {
 
-        cy.get(':nth-child(2) > div > h2').should("contain",'Pedro Pascal');
+        cy.get('@booking').then((bookingData) => {
+           const name = bookingData.name;
+           const email = bookingData.email;
+           const phone = bookingData.phone;
+           const subject = bookingData.subject;
+           const message = bookingData.message;
+
+           cy.fillBooking(name,email,phone, subject, message);
+        })
+        cy.get(':nth-child(2) > div > h2').should("contain",this.booking.name);
 
         cy.visit('https://automationintesting.online/#/admin'); 
         cy.get('@data').then((newData) => {
@@ -46,14 +29,54 @@ describe('Login feature', () => {
         })
         cy.get('#frontPageLink').should('be.visible');
         cy.get("a[href='#/admin/messages']").click();
-        cy.visit('https://automationintesting.online/#/admin/messages'); 
+        cy.visit('https://automationintesting.online/#/admin/messages');
+        cy.get('#message0 > .col-sm-2 > p');
+
+        cy.get('.col-sm-2').each(() => {
+        }).then(($list) => {
+            console.log('El tamaño de la lista es : '+$list.length);
+            expect($list).to.contain.text(this.booking.name);
+        });
     });
 
-   it('should login successfully if credentials are valid', function () {
+   it('Create Booking refactored', function () {
+        const {name,email,phone,subject,message} = this.booking;
+        cy.fillBooking(name,email,phone, subject, message);
 
-        const {username,password} = this.data.validCredentials;    // We are using destructuring
+        cy.get(':nth-child(2) > div > h2').should("contain",this.booking.name);
+        cy.visit('https://automationintesting.online/#/admin'); 
+
+        const {username,password} = this.data.validCredentials;    
         cy.login(username,password);
         cy.get('#frontPageLink').should('be.visible');
+
+        cy.get("a[href='#/admin/messages']").click();
+        cy.visit('https://automationintesting.online/#/admin/messages');
+
+        cy.get('#message0 > .col-sm-2 > p');  
+
+      //I had to add this due to a problem. If I didn't add this add the list of messages in the /admin/messages didn't showed the right values.
+      //It only showed Name and Subject as an empty List.
+
+        cy.get('.col-sm-2').each(() => {
+        }).then((list) => {
+            expect(list).to.contain.text(this.booking.name);
+        });
     });
+
+   after(() => {
+      //This will delete all the booking enquiries.
+      cy.get('span[class*="fa fa"]').click({multiple:true})
+      
+      /*
+      Try to delete only the enquiries created in this test. PENDING
+      cy.get('.col-sm-2').each(($el,index) => {
+         if($el.get(index).should('have.text',this.booking.name)){
+             cy.get('span[class*="fa fa"]').click({multiple:true})
+
+         }
+      }).then(() => {});
+      */
+   });
     
 });
